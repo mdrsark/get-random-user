bcb0cc07c4c5d9c2d8926a9f7968091b92ae6cd2
Daniel Rey <daniel.rey@wnco.com>
2020-07-23 17:10:48 -0500

    setting class properties

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -16,6 +16,7 @@ export class UserComponent implements OnInit {
   email : string;
   phone : number;
   picture : string;
+  loading : string = 'Loading';
 
   constructor(private userService: UserService) {}
 
@@ -25,6 +26,11 @@ export class UserComponent implements OnInit {
 
   getRandomUser() {
     this.userService.getRandomName().subscribe((response) => {
+      const temporalName: object = response.results[0].name;      
+      this.name = `${temporalName.title} ${temporalName.first} ${temporalName.last} `;
+      this.email = response.results[0].email;
+      this.phone = response.results[0].phone;
+      this.picture = response.results[0].picture.medium;
       console.log(response);
     });
   }
